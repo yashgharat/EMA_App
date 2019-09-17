a6e2792418261b4b2ead5ee441eb4c783078d063
yash <yashgharat12@gmail.com>
2019-09-17 00:25:15 -0400

    commit all

---
+++ app/src/main/java/com/example/ema_diary/CognitoSettings.java
@@ -0,0 +1,39 @@
+package com.example.ema_diary;
+
+import android.content.Context;
+
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.regions.Regions;
+
+public class CognitoSettings {
+    private String userPoolId = "us-east-1_CTJ2jfjlB";
+    private String clientId = "3pa7lb4mrprsaaqutoua1vvi3t";
+    private String clientSecret = "143c0dti692kj0r405bhv8l97eq0lc4su4ueejapjv1fuo11kpte";
+    private Regions cognitoRegion = Regions.US_EAST_1;
+
+    private Context context;
+
+    public CognitoSettings(Context context){
+        this.context = context;
+    }
+
+    public String getUserPoolId() {
+        return userPoolId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public Regions getCognitoRegion() {
+        return cognitoRegion;
+    }
+
+    public CognitoUserPool getUserPool(){
+        return new CognitoUserPool(context, userPoolId, clientId, clientSecret, cognitoRegion);
+    }
+}
