04f929d8d01cb5cf245ee3dc49d1d64832ac4877
yash <yashgharat12@gmail.com>
2019-09-22 18:26:55 -0400

    Pin

--- app/src/main/java/com/example/ema_diary/SplashScreen.java
+++ app/src/main/java/com/example/ema_diary/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.ema_diary;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if(UserAttributes.localPin != 0){
+            Intent i = new Intent(SplashScreen.this, PinActivity.class);
+            startActivity(i);
+        }else{
+            Intent i = new Intent(SplashScreen.this, AuthenticationActivity.class);
+            startActivity(i);
+        }
     }
 }
