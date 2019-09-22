4bd1db2b043ba42bde6818a6deaf0933c11f0a31
yash <yashgharat12@gmail.com>
2019-09-22 14:21:32 -0400

    Pin

---
+++ app/src/main/java/com/example/ema_diary/createPinActivity.java
@@ -0,0 +1,14 @@
+package com.example.ema_diary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class createPinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pin);
+    }
+}
