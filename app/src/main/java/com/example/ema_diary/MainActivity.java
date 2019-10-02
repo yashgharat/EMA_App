99caf6d9c05168d998695386a5fa2a0dd10323e7
yash <yashgharat12@gmail.com>
2019-10-02 18:56:57 -0400

    Color Scheme change

--- app/src/main/java/com/example/ema_diary/MainActivity.java
+++ app/src/main/java/com/example/ema_diary/MainActivity.java
@@ -6,13 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,19 +28,7 @@ public class MainActivity extends AppCompatActivity {
             SP.edit().putBoolean("virgin", false).apply();
         }
 
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
