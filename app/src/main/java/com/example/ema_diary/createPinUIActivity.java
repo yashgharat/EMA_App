4cca2bc5c97d1b4ef2159e327341ee0e76de0387
yash <yashgharat12@gmail.com>
2019-10-03 02:03:16 -0400

    secureLock

---
+++ app/src/main/java/com/example/ema_diary/createPinUIActivity.java
@@ -0,0 +1,14 @@
+package com.example.ema_diary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class createPinUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pin_ui);
+    }
+}
