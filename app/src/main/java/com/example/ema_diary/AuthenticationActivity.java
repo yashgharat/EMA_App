c656e44014821033d3b7878a22b43a112b003fa1
yash <yashgharat12@gmail.com>
2019-10-04 11:23:26 -0400

    cleanup

--- app/src/main/java/com/example/ema_diary/AuthenticationActivity.java
+++ app/src/main/java/com/example/ema_diary/AuthenticationActivity.java
@@ -56,8 +56,6 @@ public class AuthenticationActivity extends AppCompatActivity {
 
         final EditText editTextEmail = findViewById(R.id.email);
         final EditText editTextPassword = findViewById(R.id.password);
-//        final SwitchCompat switch_remember = findViewById(R.id.always_login);
-//        final SwitchCompat switch_quick_signIn = findViewById(R.id.quick_signIn);
 
         SP = this.getSharedPreferences("com.example.ema_diary", Context.MODE_PRIVATE);
         editor = SP.edit();
@@ -76,29 +74,6 @@ public class AuthenticationActivity extends AppCompatActivity {
 
             }
         };
-
-        //1use.setEmail(String.valueOf(editTextEmail.getText()));
-
-//        switch_quick_signIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                editor.putBoolean("quick_signIn", switch_quick_signIn.isChecked());
-//                editor.apply();
-//                //use.setQuick_signIn(switch_quick_signIn.isChecked());
-//                Log.d("SharedPrefs", String.valueOf(SP.getBoolean("quick_signIn", false)));
-//            }
-//        });
-//
-//        switch_remember.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                editor.putBoolean("remember", switch_remember.isChecked());
-//                editor.apply();
-//                //use.setRemembered(switch_remember.isChecked());
-//                Log.d("SharedPrefs", String.valueOf(SP.getBoolean("remember", false)));
-//
-//            }
-//        });
 
 
         final AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
@@ -120,21 +95,6 @@ public class AuthenticationActivity extends AppCompatActivity {
                 myIntent.putExtra("oldPass", editTextPassword.getText());
                 startActivity(myIntent);
 
-//                if(SP.getBoolean("quick_signIn", false))
-//                {
-//                    if(localPin != -1)
-//                    {
-//                        Intent myIntent = new Intent(AuthenticationActivity.this, PinActivity.class);
-//                        AuthenticationActivity.this.startActivity(myIntent);
-//                    } else{
-//                        Intent myIntent = new Intent(AuthenticationActivity.this, createPinActivity.class);
-//                        AuthenticationActivity.this.startActivity(myIntent);
-//                    }
-//                }
-//                else{
-//                    Intent myIntent = new Intent(AuthenticationActivity.this, MainActivity.class);
-//                    startActivity(myIntent);
-//                }
             }
 
             @Override
@@ -216,13 +176,5 @@ public class AuthenticationActivity extends AppCompatActivity {
             }
         });
 
-//         TextView regLink = findViewById(R.id.regLink);
-//         regLink.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View v) {
-//                 Intent i = new Intent(AuthenticationActivity.this, RegistrationActivity.class);
-//                 AuthenticationActivity.this.startActivity(i);
-//             }
-//         });
     }
 }
