33958decc2b8fc27a4544519bbb53284f4d50fac
Yash Gharat <yashgharat12@gmail.com>
2019-10-15 01:45:12 -0400

    RDS connect

--- app/src/main/java/com/example/ema_diary/RDS_Connect.java
+++ app/src/main/java/com/example/ema_diary/RDS_Connect.java
@@ -1,6 +1,9 @@
 package com.example.ema_diary;
 
+import com.amazonaws.mobileconnectors.apigateway.ApiClientFactory;
+
 public class RDS_Connect {
 
-    private ApiClientFactory
+    private ApiClientFactory factory = new ApiClientFactory();
+    final EMADiaryAPIClient client = factory.build(EMADiaryAPIClient.class);
 }
