8c1da2c90067dc1dc8094723314b87eb94c86942
yash <yashgharat12@gmail.com>
2019-10-22 10:14:11 -0400

    updatePass

--- app/src/main/java/com/example/ema_diary/CollectingInformation.java
+++ app/src/main/java/com/example/ema_diary/CollectingInformation.java
@@ -91,6 +91,7 @@ public class CollectingInformation extends AppCompatActivity {
         next2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
             }
         });
 
