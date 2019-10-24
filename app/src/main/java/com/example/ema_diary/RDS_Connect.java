13c93108f9ed21916c420abcee26a220a72b5dd9
yash <yashgharat12@gmail.com>
2019-10-24 11:51:50 -0400

    s3 upload success

--- app/src/main/java/com/example/ema_diary/RDS_Connect.java
+++ app/src/main/java/com/example/ema_diary/RDS_Connect.java
@@ -2,6 +2,9 @@ package com.example.ema_diary;
 
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -10,6 +13,9 @@ import java.nio.charset.StandardCharsets;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -23,11 +29,10 @@ public class RDS_Connect {
     private String returnStr;
     private String beginQuote = encodeValue("\""), endQuote = encodeValue("\"");
 
-    // createUser
-    private String getURL = "https://iq185u2wvk.execute-api.us-east-1.amazonaws.com/dev/";
+    // baseURL
+    private String baseURL = "https://iq185u2wvk.execute-api.us-east-1.amazonaws.com/dev/";
 
-    // update changed password
-    private String passURL = "https://iq185u2wvk.execute-api.us-east-1.amazonaws.com/dev/did-set-pw?";
+
 
 
     public RDS_Connect(){}
@@ -35,7 +40,7 @@ public class RDS_Connect {
 
     public String doGetRequest(String userid, String email) throws Exception {
 
-        String url = getURL + "user?id="+ beginQuote +
+        String url = baseURL + "user?id="+ beginQuote +
                 encodeValue(userid) + endQuote + "&email=" + beginQuote + encodeValue(email) + endQuote;
 
         getRequestHelper(url, new Callback() {
@@ -62,18 +67,8 @@ public class RDS_Connect {
 
     }
 
-    private Call getRequestHelper(String url, Callback callback){
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(callback);
-        return call;
-    }
-
     public String updatePassword(String userid) throws Exception{
-        String url = passURL + "user_id=" + beginQuote + encodeValue(userid) + endQuote;
+        String url = baseURL + "did-set-pw?user_id=" + beginQuote + encodeValue(userid) + endQuote;
         patchRequestHelper(url, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -97,10 +92,104 @@ public class RDS_Connect {
         return returnStr;
     }
 
+
+    public String uploadFile(String userid, JSONObject json) throws JSONException {
+        String url = baseURL + "create-scraped-data-url";
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+        JSONObject jgenerate= new JSONObject().put("user_id", userid).put("file_ext","json");
+
+        RequestBody rBody = RequestBody.create(JSON, jgenerate.toString(1));
+
+        postRequestHelper(url, rBody, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "call failed: " + e.toString());
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String responseStr = response.body().string();
+                    Log.i(TAG, responseStr);
+
+
+                    try {
+                        RequestBody jsonFile = RequestBody.create(JSON, json.toString(1));
+                        putRequestHelper(responseStr, jsonFile, new Callback() {
+                            @Override
+                            public void onFailure(Call call, IOException e) {
+                                Log.e(TAG, "call failed: " + e.toString());
+                            }
+
+                            @Override
+                            public void onResponse(Call call, Response response) throws IOException {
+                                if(response.isSuccessful()){
+                                    String responseStr = response.body().string();
+                                    returnStr = responseStr;
+                                    Log.i(TAG, "here" + returnStr);
+                                }
+                                else{
+                                    Log.e(TAG, "in PUT Call: " + response.toString());
+                                    Log.e(TAG, "request not successful");
+                                    Log.e(TAG, url);
+                                }
+                            }
+                        });
+                    } catch (JSONException e) {
+                        Log.e(TAG, e.toString());
+                    }
+                }
+                else{
+                    Log.e(TAG, "in POST Call: " + response.toString());
+                    Log.e(TAG, "request not successful");
+                    Log.e(TAG, url);
+                }
+            }
+        });
+
+        return returnStr;
+    }
+
+
+
     private Call patchRequestHelper(String url, Callback callback){
         Request request = new Request.Builder()
                 .url(url)
                 .patch(RequestBody.create(null, new byte[0]))
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+
+    private Call getRequestHelper(String url, Callback callback){
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    private Call postRequestHelper(String url, RequestBody rBody, Callback callback){
+        Request request = new Request.Builder()
+                .url(url)
+                .post(rBody)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    private Call putRequestHelper(String url, RequestBody rBody, Callback callback){
+        Request request = new Request.Builder()
+                .url(url)
+                .put(rBody)
                 .build();
         Call call = client.newCall(request);
         call.enqueue(callback);
