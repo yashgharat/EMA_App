627be92a8099b27b302641c8adb878083784b962
yash gharat <yashgharat12@gmail.com>
2019-10-29 10:32:20 -0400

    notification

--- app/src/main/java/com/STIRlab/ema_diary/Helpers/NotifyWorker.java
+++ app/src/main/java/com/STIRlab/ema_diary/Helpers/NotifyWorker.java
@@ -7,16 +7,19 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class NotifyWorker extends Worker {
+    private NotificationHelper pusher;
 
     public NotifyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+
+        pusher = new NotificationHelper(context);
     }
 
     @NonNull
     @Override
     public Result doWork() {
 
-
+        pusher.triggerNotif();
         return Result.success();
     }
 }
