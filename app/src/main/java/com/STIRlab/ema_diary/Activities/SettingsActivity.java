61b86eae1d337c611345a6bab4c01f6ad4140bd7
yash <yashgharat12@gmail.com>
2019-10-29 23:44:21 -0400

    Settings

---
+++ app/src/main/java/com/STIRlab/ema_diary/Activities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.STIRlab.ema_diary.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.STIRlab.ema_diary.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
