ac50fd0db9401bc668deb0af7e728307629ac51e
yash <yashgharat12@gmail.com>
2019-10-29 23:08:10 -0400

    push Notifications

--- app/src/main/java/com/STIRlab/ema_diary/Helpers/NotificationHelper.java
+++ app/src/main/java/com/STIRlab/ema_diary/Helpers/NotificationHelper.java
@@ -1,5 +1,7 @@
 package com.STIRlab.ema_diary.Helpers;
 
+import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -7,12 +9,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.SystemClock;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.STIRlab.ema_diary.Activities.MainActivity;
 import com.STIRlab.ema_diary.R;
+
+import java.util.Calendar;
 
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 
@@ -47,31 +52,37 @@ public class NotificationHelper {
         }
     }
 
-    public void triggerNotif(){
-        String notificationTitle = "Test Notification";
-        String notificationText = "This is a test Notification";
-
-        this.buildNotif();
-
-
-        Intent intent = new Intent(context, MainActivity.class);
-
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(context, 1, intent, FLAG_UPDATE_CURRENT);
-
+    private Notification getNotif(String title, String content){
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(context, test_notification_channel)
                 .setSmallIcon(R.drawable.ic__ionicons_svg_ios_create)
-                .setContentTitle(notificationTitle)
-                .setContentText(notificationText)
-                .setContentIntent(pendingIntent)
+                .setContentTitle(title)
+                .setContentText(content)
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(context);
+        return notificationBuilder.build();
+    }
 
-        notificationManager.notify(10, notificationBuilder.build());
+    private void scheduleNotification(Notification notification, int hour, int minute) {
+        Intent notificationIntent = new Intent(context, NotifyPublisher.class);
+        notificationIntent.putExtra(NotifyPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotifyPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_HOUR, pendingIntent);
+    }
+
+    public static void triggerNotif(Context context, String title, String text, int hour, int minute){
+        NotificationHelper helper = new NotificationHelper(context);
+
+        helper.scheduleNotification(helper.getNotif(title, text), hour, minute);
     }
 
 }
