fb465b6f48deb678c31409bc6b478e671abec331
yash <yashgharat12@gmail.com>
2019-11-02 13:15:56 -0400

    Help Screen

---
+++ app/src/main/java/com/STIRlab/ema_diary/Activities/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.STIRlab.ema_diary.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.STIRlab.ema_diary.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
