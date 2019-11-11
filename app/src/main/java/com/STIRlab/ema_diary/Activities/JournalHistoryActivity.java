54e61f310c50fa1421641e97e7b12f5dc8a10a0b
yash gharat <yashgharat12@gmail.com>
2019-11-11 00:15:30 -0500

    Journal history works

--- app/src/main/java/com/STIRlab/ema_diary/Activities/JournalHistoryActivity.java
+++ app/src/main/java/com/STIRlab/ema_diary/Activities/JournalHistoryActivity.java
@@ -3,16 +3,21 @@ package com.STIRlab.ema_diary.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.STIRlab.ema_diary.Helpers.JournalEntry;
 import com.STIRlab.ema_diary.Helpers.JournalEntryAdapter;
 import com.STIRlab.ema_diary.Helpers.RDS_Connect;
 import com.STIRlab.ema_diary.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 
@@ -28,9 +33,14 @@ public class JournalHistoryActivity extends AppCompatActivity {
     private List<JournalEntry> history;
 
     private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    private TextView missedCount, completeCount;
+    private String userId;
+
+    private FloatingActionButton previous;
 
     private RDS_Connect client;
-
     private SharedPreferences SP;
 
     @Override
@@ -38,21 +48,50 @@ public class JournalHistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_history);
         SP = this.getSharedPreferences("com.STIRlab.ema_diary", Context.MODE_PRIVATE);
-
+        userId = SP.getString("username", null);
         client = new RDS_Connect();
+
+        previous = findViewById(R.id.journalHistoryPrevious);
+
+        missedCount = findViewById(R.id.missedCount);
+        completeCount = findViewById(R.id.completedCount);
+        swipeRefreshLayout = findViewById(R.id.journal_swipe);
 
         recyclerView = findViewById(R.id.recyclerJournal);
         recyclerView.setHasFixedSize(false);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        init();
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                init();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void init(){
         try {
-            history = client.getJournalEntries(SP.getString("username", null));
+            history = client.getJournalEntries(userId);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        adapter = new JournalEntryAdapter(this, history);
-
+        adapter = new JournalEntryAdapter(JournalHistoryActivity.this, history);
         recyclerView.setAdapter(adapter);
+        try {
+            completeCount.setText(String.valueOf(client.getNumCompleted(userId)));
+            missedCount.setText(String.valueOf(client.getNumMissed(userId)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/STIRlab/ema_diary/Helpers/JournalEntryAdapter.java
+++ app/src/main/java/com/STIRlab/ema_diary/Helpers/JournalEntryAdapter.java
@@ -1,6 +1,8 @@
 package com.STIRlab.ema_diary.Helpers;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +10,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.STIRlab.ema_diary.R;
 
+import java.text.ParseException;
 import java.util.List;
 
 public class JournalEntryAdapter extends RecyclerView.Adapter<JournalEntryAdapter.JournalEntryViewHolder> {
@@ -37,10 +41,33 @@ public class JournalEntryAdapter extends RecyclerView.Adapter<JournalEntryAdapte
         JournalEntry entry = history.get(position);
 
         //binding the data with the viewholder views
-        holder.time.setText(entry.getFormattedTime());
-        holder.isComplete.setText(String.valueOf(entry.isComplete()));
-        holder.earnings.setText(String.valueOf(entry.gettotalEarnings()));
-        holder.increment.setText(String.valueOf(entry.getIncrementEarnings()));
+        try {
+            holder.time.setText(entry.getFormattedTime(context));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if(entry.isComplete()) {
+            holder.isComplete.setText("Complete");
+
+            Drawable drawable = context.getDrawable(R.drawable.ic_check_black_24dp);
+            drawable = DrawableCompat.wrap(drawable);
+            DrawableCompat.setTint(drawable, context.getColor(R.color.secondary));
+            holder.isComplete.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+        }
+        else{
+            holder.isComplete.setText("Missed");
+            holder.isComplete.setTextColor(context.getResources().getColor(R.color.destructive));
+
+            Drawable drawable = context.getDrawable(R.drawable.ic_close_black_24dp);
+            drawable = DrawableCompat.wrap(drawable);
+            DrawableCompat.setTint(drawable, context.getColor(R.color.destructive));
+            holder.isComplete.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+
+
+            holder.earnings.setTextColor(context.getResources().getColor(R.color.normal));
+        }
+        holder.earnings.setText("$" + entry.gettotalEarnings());
+        holder.increment.setText("+$" + entry.getIncrementEarnings());
     }
 
     @Override
