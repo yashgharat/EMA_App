120c2ca20cd556c59f1d642b110376fdd249c433
yash gharat <yashgharat12@gmail.com>
2020-02-21 00:01:36 -0500

    VIBRATION ANIMATIONS

--- app/src/main/java/com/STIRlab/ema_diary/Activities/PinActivity.java
+++ app/src/main/java/com/STIRlab/ema_diary/Activities/PinActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -30,7 +36,7 @@ public class PinActivity extends AppCompatActivity {
     private TextView title, signOut;
     private CognitoSettings cognitoSettings;
 
-
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,7 @@ public class PinActivity extends AppCompatActivity {
         SP = this.getSharedPreferences("com.STIRlab.ema_diary", Context.MODE_PRIVATE);
         cognitoSettings = new CognitoSettings(this);
 
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         title = findViewById(R.id.title_pin);
         signOut = findViewById(R.id.pin_signout);
@@ -85,19 +92,41 @@ public class PinActivity extends AppCompatActivity {
         inputPin.setOnTextCompleteListener(new PinField.OnTextCompleteListener() {
             @Override
             public boolean onTextComplete(@NotNull String str) {
-                if(str.equals(localPin)){
+                if (str.equals(localPin)) {
                     getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
                     finish();
                     Intent main = new Intent(PinActivity.this, MainActivity.class);
                     startActivity(main);
 
                 } else {
-                    showDialogMessage("Alert", "Pin not recognized", false);
+                    Log.e(TAG, localPin);
+
+                    Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+                    incorrectVibrate();
+                    inputPin.startAnimation(shake);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            inputPin.setText("");
+
+                        }
+                    }, 1000);
 
                 }
                 return false;
             }
         });
+    }
+
+    private void incorrectVibrate() {
+        if (vibrator != null && vibrator.hasVibrator())
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                VibrationEffect effect = VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE);
+                vibrator.vibrate(effect);
+            } else {
+                vibrator.vibrate(500);
+            }
     }
 
     private void showDialogMessage(String title, String body, final boolean exitActivity) {
@@ -116,7 +145,7 @@ public class PinActivity extends AppCompatActivity {
             }
         });
         userDialog = builder.create();
-        userDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        userDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
         userDialog.show();
 
     }
